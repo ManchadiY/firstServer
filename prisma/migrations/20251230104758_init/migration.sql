f86782ab3c2ca567abf72ff8a57f8fcfa78ae292
Yuvraj <yuvrajmanchadi321@gmail.com>
2025-12-30 18:28:41 +0530

    prisma changed to supabase

--- prisma/migrations/20251230104758_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Request" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "method" TEXT NOT NULL,
-    "url" TEXT NOT NULL,
-    "headers" TEXT NOT NULL,
-    "body" TEXT NOT NULL
-);
